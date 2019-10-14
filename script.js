cad03385f0cee7dfc856985c055bdd6a25ed8acc
Kin Au <kinnamau@gmail.com>
2019-10-14 14:17:16 +0100

    calculation functions fixed, styling added

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Features to add:
+// - set display length character limit for displayCurrent
+// - displayHistory should show latest history when overflow
+// 
+// All working, but code is complicated/hard to read. For refactor:
+// - should not use eval(), replace with if/switch statement that points to different functions for each operator (or dispatch table - a data structure that holds functions)
+// - start by writing the calculation functions
+// - rewrite numberClicked function (maybe with switch statement and regex?)
+// - use "let lastButtonClicked" (e.g. can check if user clicks another operator and will replace selectedOperator, and no need to check for "=" in displayHistory.innerHTML)
+
+
 let displayHistory = document.getElementById("displayHistory");
 let displayCurrent = document.getElementById("displayCurrent");
 let numberButtonArea = document.getElementById("numberButtonArea");
@@ -11,6 +22,7 @@ let valueTwo = "";
 let valueCalculated = 0;
 let selectedOperator = "";
 let lastSelectedOperator = "";
+let operatorActive = false;
 
 numberButtonArea.addEventListener("click", numberClicked);
 operatorArea.addEventListener("click", operatorClicked);
@@ -19,18 +31,21 @@ clearButton.addEventListener("click", clear);
 
 function clear() {
     valueCurrent = "0";
+    valueOne = "";
+    valueTwo = "";
     valueCalculated = 0;
+    selectedOperator = "";
+    lastSelectedOperator = "";
     displayHistory.innerHTML = "";
     displayCurrent.innerHTML = valueCurrent;
 }
 
 function numberClicked(event) {
-// this whole function is ugly, rewrite it (maybe with switch statement and regex?)
     if (displayHistory.innerHTML.charAt(displayHistory.innerHTML.length-1) === "=") {
         clear();
     }
     // IF statement checks whether clicked element was a button (not blank space)
-    if (event.target.className === "numberButton") {
+    if (event.target.className.includes("numberButton")) {
         // IF statement checks if decimal already exists - is there a built-in method/function to check if a valid number? Use parseFloat?
         if (event.target.innerHTML === "." && valueCurrent.includes(".")) {
             // do nothing
@@ -48,52 +63,51 @@ function numberClicked(event) {
         }
     }
     valueOne = parseFloat(valueCurrent);
+    operatorActive = false;
 }
 
-function calculateValue(selectedOperator) {
-    // I need to make this calculate...
-    valueCalculated = eval(valueTwo + selectedOperator + valueOne);
+function calculateValue(operator) {
+    valueCalculated = eval(valueTwo + operator + valueOne);
     displayCurrent.innerHTML = valueCalculated;
     valueTwo = valueCalculated;
 }
 
-function operatorClicked(event) {
-    if (displayHistory.innerHTML.charAt(displayHistory.innerHTML.length-1) === "=") {
-        displayHistory.innerHTML = displayHistory.innerHTML.slice(0, displayHistory.innerHTML.length-2);
+function equalsClicked() {
+    if (valueOne !== "" && valueOne !== 0 && valueTwo !== "") {
+        calculateValue(selectedOperator);
+        lastSelectedOperator = selectedOperator;
+        displayHistory.innerHTML += (" " + valueCurrent + " =");
+        valueOne = "";
+        valueCurrent = "0";
     }
-    selectedOperator = event.target.innerHTML;
-    if (event.target.className === "operatorButton" && valueCurrent !== "0") {
-        if (valueTwo === "") {
+}
+
+function operatorClicked(event) {
+    if (event.target.className.includes("operatorButton")) {
+        lastSelectedOperator = selectedOperator;
+        selectedOperator = event.target.innerHTML;
+        if (valueTwo === "" && valueCurrent !== "0") {
             displayHistory.innerHTML += (displayCurrent.innerHTML + " " + selectedOperator);
             valueCurrent = "0";
             displayCurrent.innerHTML = valueCurrent;
+            operatorActive = true;
             valueTwo = valueOne;
             valueOne = "";
-        } else if (valueTwo !== "") {
-            displayHistory.innerHTML += " " + valueOne + " " + selectedOperator;
-            calculateValue(selectedOperator);
-            valueCurrent = "0";
+        } else if(valueTwo !== "") {
+            if (displayHistory.innerHTML.charAt(displayHistory.innerHTML.length-1) === "=") {
+                displayHistory.innerHTML = displayHistory.innerHTML.slice(0, displayHistory.innerHTML.length-2);
+                displayHistory.innerHTML += " " + valueOne + " " + selectedOperator;
+                operatorActive = true;
+            } else if (operatorActive && (displayHistory.innerHTML.charAt(displayHistory.innerHTML.length-1) === "+" || "-" || "*" || "/")) {
+                displayHistory.innerHTML = displayHistory.innerHTML.slice(0, displayHistory.innerHTML.length-2);
+                displayHistory.innerHTML += " " + selectedOperator;
+            }
+            else if (valueCurrent !== "0") {
+                calculateValue(lastSelectedOperator);
+                valueCurrent = "0";
+                displayHistory.innerHTML += " " + valueOne + " " + selectedOperator;
+                operatorActive = false;
+            }
         }
     }
 }
-
-function equalsClicked() {
-    if (valueOne !== "" && valueTwo !== "") {
-        lastSelectedOperator = selectedOperator;
-        calculateValue(selectedOperator);
-        displayHistory.innerHTML += (" " + valueCurrent + " =");
-        valueOne = "";
-    }
-}
-
-
-// if/switch statement that points to different functions for + - * /
-// dispatch table - a data structure that holds functions
-
-
-
-
-
-
-
-
